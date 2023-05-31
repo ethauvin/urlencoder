8bf5bb8349defc38fd411e7a6bd0909dbdfd04cf
Erik C. Thauvin <erik@thauvin.net>
2023-05-31 15:20:31 -0700

    Updated more dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
 }
 
 dependencies {
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.44.0")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.46.0")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.0")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.10")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
     implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.0")
-    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.5.0.2730")
+    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.2.0.3129")
 }
