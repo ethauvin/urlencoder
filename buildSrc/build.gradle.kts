32e21491c7286156d8263fcfb3c6d3b88ebe0c6e
Erik C. Thauvin <erik@thauvin.net>
2023-09-03 14:41:05 -0700

    Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
 
 dependencies {
     implementation("com.github.ben-manes:gradle-versions-plugin:0.47.0")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.0")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.1")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.1")
+    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.3")
 }
 
 dependencyLocking {
