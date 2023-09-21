392af12f50fef6f50da7c2b6883ba1192297fc51
Erik C. Thauvin <erik@thauvin.net>
2023-09-21 10:28:32 -0700

    Now using in memory PGP key
    Fixed signin syntax
    
    Fixed dangling char
    
    Fixed typo
    
    Added GPG key password

--- buildSrc/src/main/kotlin/buildsrc/conventions/publishing.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/conventions/publishing.gradle.kts
@@ -85,7 +85,10 @@ publishing {
 }
 
 signing {
-    useGpgCmd()
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+
     sign(publishing.publications)
 
     setRequired({
