b59d01ec6d9de4819498b61cfb10c4bb7d31fbe7
Erik C. Thauvin <erik@thauvin.net>
2024-03-26 14:13:32 -0700

    Bumped Dokka to version 1.9.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation("com.github.ben-manes:gradle-versions-plugin:0.51.0")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.1")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.4")
 }
