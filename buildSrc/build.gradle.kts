d66fdb12e69a389af7c6a60f9fedd6f69d7219ab
Erik C. Thauvin <erik@thauvin.net>
2024-10-15 12:43:01 -0700

    Updated dependencies
    Bumped Detekt to version 1.23.7
    Bumped Kover to version 0.8.3
    Bumped Kotlin to version 1.9.24

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 
 dependencies {
     implementation("com.github.ben-manes:gradle-versions-plugin:0.51.0")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.1")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.7")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.4")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.8.3")
     implementation("org.apache.httpcomponents:httpclient:4.5.13")
 }
